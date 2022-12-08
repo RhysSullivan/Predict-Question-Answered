df01cfc67ca5d248bcb98b23daaf887e62426068
Ben Roberts <61424934+benjaminRoberts01375@users.noreply.github.com>
2022-12-08 01:45:59 -0500

    Add normalized output

--- checkWeights.py
+++ checkWeights.py
@@ -1,19 +1,15 @@
 import tensorflow as tf
 
-# Load the model
-model = tf.keras.models.load_model("model.h5")
+model = tf.keras.models.load_model("model.h5") # Load the model
 
-# Get the weights of the input layer
-weights = model.weights[0]
+weights = model.weights[0] # Get the weights of the input layer
 
-# Convert the weights to a numpy array
-weights_array = weights.numpy()
+weights_array = weights.numpy() # Convert to array
 
-# Get the number of input neurons and output neurons
-num_inputs = weights_array.shape[0]
-num_outputs = weights_array.shape[1]
+num_inputs = weights_array.shape[0] # Number of input neurons
+num_outputs = weights_array.shape[1] # Number of hidden output neurons
 
-# Print the weight values in a table
+# Print the weight values with formatting
 print("Input Neuron".ljust(15) + "Output Neuron".ljust(15) + "Weight Value")
 for i in range(num_inputs):
   for j in range(num_outputs):
@@ -21,12 +17,11 @@ for i in range(num_inputs):
     print(str(i).ljust(15) + str(j).ljust(15) + str(weight))
 
 
-# Importance
+# Importance of each input
 importances = []
 for i in range(num_inputs):
-  # Average the absolute values of the weights for this input neuron
-  importance = sum(abs(weights_array[i])) / num_outputs
-  importances.append(importance)
+  importances.append(round((sum(abs(weights_array[i])) / num_outputs), 8)) # Average the absolute values of the weights for this input neuron
 
 # Print the importances
-print(importances)
+print("Not normal: " + str(importances).replace(",", "")) # Cleanup output
+print("Normalized: " + str(tf.keras.utils.normalize(importances, axis=-1)[0])) # Normalize and cleanup output
